9c17f7299167999fad152f956260266648f0dfe0
Yihan <yihanwang@amss.ac.cn>
2022-07-03 21:09:42 +0800

    Update pgd.py

--- CIFAR10/utils/pgd.py
+++ CIFAR10/utils/pgd.py
@@ -47,3 +47,25 @@ def pgd_2(model, data, target, epsilon=1.5, alpha=0.2, steps=10, rand_init=False
     model.train(training_mode)
     adv_data = data.data + delta.data
     return adv_data
+
+
+def trades_pgd_inf(model, data, target=None, epsilon=8/255, alpha=2/255, steps=10, random_start=True):
+    training_mode = model.training
+    model.eval()
+    delta = torch.zeros_like(data, requires_grad=True)
+    if random_start:
+        delta.data.uniform_(-epsilon, epsilon)
+    for _ in range(steps):
+        with torch.enable_grad():
+            loss = F.kl_div(
+                F.log_softmax(model(data+delta), dim=1),
+                F.softmax(model(data), dim=1),
+                reduction='batchmean'
+            )
+        grad = torch.autograd.grad(loss, [delta])[0].data
+        delta.data = delta.data + alpha*torch.sign(grad)
+        delta.data = torch.clamp(delta.data, -epsilon, epsilon)
+        delta.data = torch.clamp(data.data+delta.data, 0, 1) - data.data
+    adv_data = delta.data + data.data
+    model.train(training_mode)
+    return adv_data
