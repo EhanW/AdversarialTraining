9399ba04d594e2e7f2836e0bb4c6e1099dd31e7f
Yihan <yihanwang@amss.ac.cn>
2022-07-07 19:21:47 +0800

    Update lenet.py

--- MNIST/networks/lenet.py
+++ MNIST/networks/lenet.py
@@ -8,19 +8,19 @@ class LeNet(nn.Module):
         super(LeNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(256, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv3 = nn.Conv2d(16, 120, 5)
+        self.fc1 = nn.Linear(120, 84)
+        self.fc2 = nn.Linear(84, 10)
         self.maxpool = nn.MaxPool2d(2)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         x = self.maxpool(self.relu(self.conv1(x)))
         x = self.maxpool(self.relu(self.conv2(x)))
-        x = x.view(-1, 256)
+        x = self.relu(self.conv3(x))
+        x = x.view(-1, 120)
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
-        x = self.relu(self.fc3(x))
         return x
 
 
